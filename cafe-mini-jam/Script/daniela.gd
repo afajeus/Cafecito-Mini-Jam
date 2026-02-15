03926ba5c2d0be66af2c6257c4a5bf72abdfddf2
afajeus <afajeus@hotmail.com>
2026-02-15 17:25:44 -0400

    funcionando ahora hacer que sevea presetable

--- cafe-mini-jam/Script/daniela.gd
+++ cafe-mini-jam/Script/daniela.gd
@@ -4,7 +4,7 @@ extends CharacterBody2D
 const SPEED = 300.0
 
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	var Direccion = Input.get_axis("ui_left","ui_right")
 	
 	if Direccion:
@@ -13,10 +13,3 @@ func _physics_process(delta: float) -> void:
 		velocity.x =move_toward(velocity.x,0,SPEED)
 	move_and_slide()
 	
-	
-func _on_area_2d_area_entered(area: Area2D) -> void:
-	pass # Replace with function body.
-
-
-func _on_area_2d_area_exited(area: Area2D) -> void:
-	pass # Replace with function body.
--- cafe-mini-jam/Script/npc.gd
+++ cafe-mini-jam/Script/npc.gd
@@ -1,18 +1,33 @@
 extends Area2D
 
 const DIALGUE_1 = preload("uid://yhl2sn6ugsku")
-
 var Si_Daniela_cerca = false
+var Si_Dialogue_Activo = false
 
-func _process(delta: float) -> void:
-	if Si_Daniela_cerca and Input.is_action_just_pressed("ui_accept"):
+func _ready() -> void:
+	DialogueManager.dialogue_started.connect(_on_dialogue_started)
+	DialogueManager.dialogue_ended.connect(_on_dialogue_ended)
+
+
+func _process(_delta: float) -> void:
+	if Si_Daniela_cerca and Input.is_action_just_pressed("ui_accept") and not Si_Dialogue_Activo:
 			DialogueManager.show_dialogue_balloon(DIALGUE_1)
+		
 
-func _on_area_entered(area: Area2D) -> void:
+
+func _on_area_entered(_area: Area2D) -> void:
 	Si_Daniela_cerca = true
 	pass # Replace with function body.
 
 
-func _on_area_exited(area: Area2D) -> void:
+
+func _on_area_exited(_area: Area2D) -> void:
 		Si_Daniela_cerca = false
 	# Replace with function body.
+	
+func _on_dialogue_started(dialogue):
+	Si_Dialogue_Activo = true
+	
+func _on_dialogue_ended(dialogue):
+	await get_tree().create_timer(0.1).timeout
+	Si_Dialogue_Activo = false
