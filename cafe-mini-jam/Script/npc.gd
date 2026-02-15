35d057802041aa135edef916760c17375ec64ff3
afajeus <afajeus@hotmail.com>
2026-02-15 03:18:47 -0400

    precisando la idea

---
+++ cafe-mini-jam/Script/npc.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+const DIALGUE_1 = preload("uid://yhl2sn6ugsku")
+var si_Barra_cerca
+var animation = $AnimatedSprite2D.play("Alegre")
+# Called when the node enters the scene tree for the first time.
+
+
+func _on_area_entered(area: Area2D) -> void:
+	pass # Replace with function body.
+
+
+func _on_area_exited(area: Area2D) -> void:
+	pass # Replace with function body.
