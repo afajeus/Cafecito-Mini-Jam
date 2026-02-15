e3f640b8968ffe2c3943d0d952e1be9cbe302271
afajeus <afajeus@hotmail.com>
2026-02-15 15:12:49 -0400

    Actulizacion

--- cafe-mini-jam/Script/npc.gd
+++ cafe-mini-jam/Script/npc.gd
@@ -1,14 +1,18 @@
 extends Area2D
 
 const DIALGUE_1 = preload("uid://yhl2sn6ugsku")
-var si_Barra_cerca
-var animation = $AnimatedSprite2D.play("Alegre")
-# Called when the node enters the scene tree for the first time.
 
+var Si_Daniela_cerca = false
+
+func _process(delta: float) -> void:
+	if Si_Daniela_cerca and Input.is_action_just_pressed("ui_accept"):
+			DialogueManager.show_dialogue_balloon(DIALGUE_1)
 
 func _on_area_entered(area: Area2D) -> void:
+	Si_Daniela_cerca = true
 	pass # Replace with function body.
 
 
 func _on_area_exited(area: Area2D) -> void:
-	pass # Replace with function body.
+		Si_Daniela_cerca = false
+	# Replace with function body.
